6829ea083d2f7ce07824193fcddbda9e6c86b525
Chris Battarbee <cbattarbee@palantir.com>
2021-10-17 23:04:20 +0100

    Set up structure

---
+++ org-pomodoro.el
@@ -0,0 +1,9 @@
+;;; org-pomodoro.el --- Pomodoro with support for org mode
+;;; Commentary:
+;;; A package which allows you to perform the pomodoro methodology using your org mode TODOs as input
+
+;;; Code:
+
+
+(provide 'org-pomodoro)
+;;; org-pomodoro.el ends here
